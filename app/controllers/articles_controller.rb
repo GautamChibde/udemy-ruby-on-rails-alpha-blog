1a7d0323e61160fcc60ce16411c309207e4356e0
gautamchibde <gautamchibde@gmail.com>
2018-03-11 19:09:37 +0530

    compplete auth system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+    before_action :require_user, except: [:index, :show]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def new
         @article = Article.new
@@ -9,7 +12,7 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
+
     end
 
     def create 
@@ -23,7 +26,6 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Successfully updated..."
             redirect_to article_path(@article)
@@ -33,17 +35,28 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         flash[:notice] = "Successfully deleted..."
         redirect_to articles_path
     end
+
     def show
-        @article = Article.find(params[:id])
+
     end
 
     private
         def article_params
             params.require(:article).permit(:title, :description)
+        end
+
+        def set_article
+            @article = Article.find(params[:id])
+        end
+
+        def require_same_user
+            if current_user != @article.user
+                flash[:danger] = "You can only edit or delete your own"
+                redirect_to root_path
+            end
         end
 end
